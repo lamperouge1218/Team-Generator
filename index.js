55767b952425f4054b79832e6341c273703fbadb
lamperouge <philbohn1791@gmail.com>
2021-10-02 12:05:02 -0400

    added starting pseudocode to lib files with classes

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-
-// Employee must have Name , ID, email, getName(), getID, getEmail(), getRole(returns employee)
-
-// Manager, inlcuded employee + office number override getRole(show manager)
-
-// Engineer employee + GitHub user name, getGithub, getRole(show Engineer)
-
-// Intern emplee + school getSchool(), getRole(Intern)
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
 
 // add validation to each prompt to ensure proper user input
 
