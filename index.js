ad7df644fc0c468b70029ae721d7753a86b84044
lamperouge <philbohn1791@gmail.com>
2021-10-04 16:46:49 -0400

    filled out Employee questions regarding parameters

--- index.js
+++ index.js
@@ -17,5 +17,85 @@ const employeePrompt = [
         choices: ["Manager", "Engineer", "Intern"],
     },
 
-    
+    {
+        type: "input",
+        message: "Enter Team Member name",
+        name: "name",
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("Please enter Team Member name");
+            }
+            return true;
+        }
+    },
+
+    {
+        type: "input",
+        message: "Enter Team Member ID",
+        name: "id",
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("Please enter Team Member ID number");
+            }
+            return true;
+        }
+    },
+
+    {
+        type: "input",
+        message: "Enter Team Member email address",
+        name: "email",
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("Please enter Team Member email address");
+            }
+            return true;
+        }
+    },
+
+    {
+        type: "input",
+        message: "Enter team Manager office number",
+        name: "officeNumber",
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("Please enter team Manager office number");
+            }
+            return true;
+        },
+        when: (answer) => answer.role === "Manager",
+    },
+
+    {
+        type: "input",
+        message: "Enter GitHub username of new Engineer",
+        name: "gitHub",
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("Please enter GitHub username of new Engineer");
+            }
+            return true;
+        },
+        when: (answer) => answer.role === "Engineer",
+    },
+
+    {
+        type: "input",
+        message: "Enter School name of Intern",
+        name: "school",
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("Please enter School name of Intern");
+            }
+            return true;
+        },
+        when: (answer) => answer.role === "Intern",
+    },
+
+    {
+        type: "confirm",
+        name: "confirmAddEmployee",
+        message: "Would you like to add more team members?",
+        default: false,
+      },    
 ]
