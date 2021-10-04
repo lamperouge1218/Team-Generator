cc8c5b2f4950c3fc7f9f2eb3804cab58b1ca141f
lamperouge <philbohn1791@gmail.com>
2021-10-04 19:49:09 -0400

    started filling out the HTML generation

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,9 +1,11 @@
+const Employee = require("../lib/Employee");
+
 // create Manager Card
 const createManagerCard  = (employee) => {
     return `<div class="card text-center bg-info" style="width: 18rem;">
     <div class="card-body">
         <h4 class="card-title">${employee.name}</h4>
-        <h5 class="card-subtitle mb-2 text-warning">${employeeInfo.role}</h5>
+        <h5 class="card-subtitle mb-2 text-warning">Manager</h5>
         <h6 class="card-subtitle mb-2 text-dark">ID: ${employee.id}</h6>
         <p class="card-text">Office Number: ${employee.officeNumber}</p>
         <a href="mailto:${employee.email}" class="card-link text-dark">Email: ${employee.email}</a>
@@ -16,7 +18,7 @@ const createEngineerCard = (employee) => {
     return `<div class="card text-center bg-success" style="width: 18rem;">
     <div class="card-body">
         <h4 class="card-title">${employee.name}</h4>
-        <h5 class="card-subtitle mb-2 text-warning">${employeeInfo.role}</h5>
+        <h5 class="card-subtitle mb-2 text-warning">Engineer</h5>
         <h6 class="card-subtitle mb-2 text-dark">ID: ${employee.id}</h6>
         <a href="https://github.com/${employee.gitHub}" target="_blank" class="text-dark">GitHub: ${employee.gitHub}</a>
         <a href="mailto:${employee.email}" class="text-dark">Email: ${employee.email}</a>
@@ -29,12 +31,26 @@ const createInternCard = (employee) => {
     return `<div class="card text-center bg-secondary" style="width: 18rem;">
     <div class="card-body">
         <h4 class="card-title">${employee.name}</h4>
-        <h5 class="card-subtitle mb-2 text-warning">${employeeInfo.role}</h5>
+        <h5 class="card-subtitle mb-2 text-warning">Intern</h5>
         <h6 class="card-subtitle mb-2 text-dark">ID: ${employee.id}</h6>
         <p class="card-text">School: ${employee.school}</p>
         <a href="mailto:${employee.email}" class="card-link text-dark">Email: ${employee.email}</a>
     </div>
 </div>`;
+}
+
+generateHTML = (data) => {
+    const cardArray = [];
+
+    for (let i = 0; i < data.length; i++) {
+        const employee = data[i];
+        const role = Employee.getRole();
+
+        if (role === "Manager") {
+            const managerCard = createManagerCard(employee);
+            cardArray.push(managerCard);
+        }
+    }
 }
 
 module.exports = generateHTML;
